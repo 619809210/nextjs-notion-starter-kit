27dd3fb19f6e524c73a49ce7b63e35c8c4bba3e7
Carina <30744174+619809210@users.noreply.github.com>
2022-05-17 22:04:33 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: 'carinaself',
+  rootNotionPageId: 'cfdf466a793c4eafa18800736807203a',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -10,7 +10,7 @@ export default siteConfig({
 
   // basic site info (required)
   name: '小饼的个人网站',
-  domain: 'https://www.notion.so/carinaself',
+  domain: 'https://carinaself.notion.site/Home-cfdf466a793c4eafa18800736807203a',
   author: 'Carina',
 
   // open graph metadata (optional)
